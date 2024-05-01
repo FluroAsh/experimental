1a70c3e738b63b291d6c473caa3b839ec1d61887
Ashley Thompson <workashleythompson@gmail.com>
2024-05-01 20:42:27 +1000

    Add not null constraints on all User fields except Last Name

--- backend/express-auth/server/src/db/schema.ts
+++ backend/express-auth/server/src/db/schema.ts
@@ -4,8 +4,8 @@ export const dbSchema = pgSchema('foo_bar')
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  username: varchar('username', { length: 255 }).unique(),
-  password: varchar('password', { length: 255 }),
-  firstName: text('first_name'),
+  username: varchar('username', { length: 255 }).unique().notNull(),
+  password: varchar('password', { length: 255 }).notNull(),
+  firstName: text('first_name').notNull(),
   lastName: text('last_name')
 })
